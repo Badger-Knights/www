c86cedf8ace0d3e530bc5304e4c66376174ee918
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2025-05-27 01:21:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BadgersKnightsSite from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BadgersKnightsSite />
+  </React.StrictMode>
+);
 
