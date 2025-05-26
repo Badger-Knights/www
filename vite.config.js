0b365c2bae10f370c795a4b35cbb40cc42fe49c5
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2025-05-27 00:32:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/www/', // Ensure this matches your GitHub repo name
+  plugins: [react()],
+});
 
